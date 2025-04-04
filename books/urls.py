7f8d3186116b74a9bc04339e698efbbba463b1ed
Maria <marusjakarassmaria789@gmail.com>
2025-04-04 11:45:59 +0300

    updated login/reg urls

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 from .views import BookCreateView, BookUpdateView
@@ -11,7 +10,7 @@ urlpatterns = [
     path('book-of-the-day/', views.book_of_the_day, name='book_of_the_day'),
     path('add/', BookCreateView.as_view(), name='book_add'),
     path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout_view, name='logout'), 
 ]
