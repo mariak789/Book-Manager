96a3379a116ab4e1704394fa07f3f569aecb6de8
Maria <marusjakarassmaria789@gmail.com>
2025-04-04 11:39:26 +0300

    added forms for registration and login

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,31 @@
 from django import forms
 from .models import Book
+from django.contrib.auth.models import User
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date', 'description', 'quote', 'cover']
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirmation = cleaned_data.get('password_confirmation')
+
+        if password != password_confirmation:
+            raise forms.ValidationError("Паролі не співпадають.")
+        return cleaned_data
+    
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
